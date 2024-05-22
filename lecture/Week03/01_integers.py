c3e8f93719a51c4ba6a459afd0d0a03ef0900c66
ZacharyBad <zachary.abad@benilde.edu.ph>
2024-05-22 19:19:02 +0800

    added lecture Week3

---
+++ lecture/Week03/01_integers.py
@@ -0,0 +1,21 @@
+class_size = 9
+students_present = 5
+students_surnames = [
+    "Abad",
+    "Hermoza",
+    "Gaza",
+    "Malanog",
+    "Vinson",
+]
+appdaet = {
+    "title" : "Application Development and Emerging Technologies",
+    "prerequisite_to": "APPINTR"
+}
+print(type(class_size))
+print(type(students_present))
+print(type(students_surnames))
+print(type(appdaet))
+# snake_case (usually in Python)
+# camelCase (Javascript)
+# PascalCase (c#,Java)
+# kebab-case (URLs)
