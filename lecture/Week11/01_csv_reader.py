872d19f13b5b35778f3e17295636be20465bb08a
ZacharyBad <144414135+ZacharyBad@users.noreply.github.com>
2024-07-31 18:56:35 +0800

    Update 01_csv_reader.py

--- lecture/Week11/01_csv_reader.py
+++ lecture/Week11/01_csv_reader.py
@@ -1,6 +1,20 @@
 import csv
 
+axis = []
+allies = []
+
 with open("ww2_countries.csv") as csvfile:
   csvreader = csv.reader(csvfile)
   for row in csvreader:
-    print(row[1])
+    if row[2] == "Allies":
+      allies.append(row)
+    elif row[2] == "Axis":
+      axis.append(row)
+
+print("Axis: ")
+print("-" * 30)
+print("\n".join([country[1] for country in axis]))
+
+print("Allies: ")
+print("-" * 30)
+print("\n".join([country[1] for country in allies]))
